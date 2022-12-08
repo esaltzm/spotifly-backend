7528130f4e9567e923a8cea87f88b9eca30374f0
aaixn <axnguyen246@gmail.com>
2022-12-08 08:17:32 -0800

    comment out post and delete for now

--- controllers/User.js
+++ controllers/User.js
@@ -24,24 +24,24 @@ router.get('/:username', async (req, res, next) => {
     }
 })
 
-router.post('/', async (req, res, next) => {
-    try {
-        const newUserInfo = req.body
-        const newUser = await User.create(newUserInfo)
-        res.json(newUser)
-    }
-    catch (err) {
-        next(err)
-    }
-})
+// router.post('/', async (req, res, next) => {
+//     try {
+//         const newUserInfo = req.body
+//         const newUser = await User.create(newUserInfo)
+//         res.json(newUser)
+//     }
+//     catch (err) {
+//         next(err)
+//     }
+// })
 
-router.delete('/:username', async (req, res, next) => {
-    try {
-        const username = req.params.username
-        const deleteUser = await User.findOneAndDelete({username: username})
-        res.json(deleteUser)
-    }
-    catch {
-        next(err)
-    }
-})
+// router.delete('/:username', async (req, res, next) => {
+//     try {
+//         const username = req.params.username
+//         const deleteUser = await User.findOneAndDelete({username: username})
+//         res.json(deleteUser)
+//     }
+//     catch {
+//         next(err)
+//     }
+// })
