3b50b3ba61aa5f36ee7828f980688b90ec8bda00
mgreen1092 <morgan.green0209@gmail.com>
2022-12-08 10:15:41 -0500

    post and delete router to song.js controller

--- controllers/Song.js
+++ controllers/Song.js
@@ -1,3 +1,23 @@
 const express=require('express')
 const router=express.Router()
 const Song=require('../models/Song')
+
+router.post('/playlist/:id', async (req, res) => {
+    try {
+        const newSong = await Song.create(req.body)
+        res.status(201).json(newSong)
+    } catch (err) {
+        next(err)
+    }
+})
+
+router.delete('/song/:id', async (req, res)=> {
+    try {
+        const deleteSong = await Song.findOneAndDelete({_id: req.params.id})
+        if (deleteSong) {
+            res.sendStatus(204)
+        }
+    } catch (err) {
+        next(err)
+    }
+})
