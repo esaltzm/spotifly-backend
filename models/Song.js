7b0789122b4cf45674cbace781139b57c862c6e0
esaltzm <elisaltzman@gmail.com>
2022-12-07 20:52:54 -0500

    song model

--- models/Song.js
+++ models/Song.js
@@ -1,1 +1,18 @@
-//song model
+const mongoose = require("../db/connection");
+const Schema = mongoose.Schema;
+
+const songSchema = new Schema({
+    name: String,
+    artist: String,
+    album: String,
+    duration: Number, // in seconds
+    genre: String,
+    spotifyID: String,
+    playlists: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Playlist'
+    }]
+}, { timestamps: true })
+
+const Song = mongoose.model('Song', songSchema)
+module.exports = Song
