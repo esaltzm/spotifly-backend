57a1411a29da0e63ff19e369063ecda7516fe2a7
esaltzm <elisaltzman@gmail.com>
2022-12-07 20:35:25 -0500

    user model

--- models/User.js
+++ models/User.js
@@ -1,1 +1,13 @@
-//user model
+const mongoose = require("../db/connection");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: String,
+    playlists: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Playlist'
+    }]
+})
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
