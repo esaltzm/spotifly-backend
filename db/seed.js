efc9a5c9cac9cd1580d99297def1b62264f2e94d
esaltzm <elisaltzman@gmail.com>
2022-12-08 11:37:50 -0500

    seed users only

--- db/seed.js
+++ db/seed.js
@@ -1,1 +1,17 @@
-//seed db
+const User = require('../models/User')
+// const Playlist = require('../models/Playlist')
+// const Song = require('../models/Song')
+const data = require('./seedData.json')
+
+const users = data.map(user => {
+    return {
+        username: user.username,
+        playlists: []
+    }
+})
+
+User.deleteMany()
+  .then(() => User.insertMany(users))
+  .then(console.log)
+  .catch(console.error)
+  .finally(process.exit)
