f3b8f947c9033c3d29f20f3bc70bf83365356fbf
esaltzm <elisaltzman@gmail.com>
2022-12-07 20:45:48 -0500

    playlist model

--- models/Playlist.js
+++ models/Playlist.js
@@ -1,1 +1,13 @@
-//playlist model
+const mongoose = require("../db/connection");
+const Schema = mongoose.Schema;
+
+const playlistSchema = new Schema({
+    name: String,
+    songs: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Song'
+    }]
+}, { timestamps: true })
+
+const User = mongoose.model('User', userSchema)
+module.exports = User
