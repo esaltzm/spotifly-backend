566416dc576d42e6fa22026d680ce7622ee29fea
esaltzm <elisaltzman@gmail.com>
2022-12-08 16:17:42 -0500

    populate users with playlists

--- controllers/User.js
+++ controllers/User.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.get('/', async (req, res, next) => {
     try {
-        const users = await User.find({})
+        const users = await User.find({}).populate('playlists')
         res.json(users)
     }
     catch (err) {
