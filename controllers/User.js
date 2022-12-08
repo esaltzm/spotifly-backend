b37d74005fa14c626aba89bc9b8023dd62df9b7d
aaixn <axnguyen246@gmail.com>
2022-12-08 07:26:30 -0800

    get all users route

--- controllers/User.js
+++ controllers/User.js
@@ -1,1 +1,14 @@
 //controller
+const express = require('express')
+const User = require('../models/User')
+const router = express.Router()
+
+router.get('/', async (req, res, next) => {
+    try {
+        const users = await User.find({})
+        res.json(users)
+    }
+    catch (err) {
+        next(err)
+    }
+})
