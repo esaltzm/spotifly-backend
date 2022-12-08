7cc48eaf7f2467168ea7e2ae944c97b4045f1dc7
esaltzm <elisaltzman@gmail.com>
2022-12-08 09:46:35 -0500

    initial playlist controller

--- controllers/Playlist.js
+++ controllers/Playlist.js
@@ -1,1 +1,14 @@
-//controller
+const express = require('express')
+const Playlist = require('../models/Playlist')
+const router = express.Router()
+
+router.get('/playlists', async (req, res, next) => {
+    try {
+        const playlists = await Playlist.find({})
+        res.json(playlists)
+    } catch (err) {
+        next(err)
+    }
+})
+
+module.exports = router
