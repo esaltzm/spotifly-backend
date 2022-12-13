9074aeedc8a587ca1e9d3e1f694d00779322cbf9
esaltzm <elisaltzman@gmail.com>
2022-12-13 10:05:10 -0500

    use firebase auth in index.js

--- index.js
+++ index.js
@@ -3,13 +3,29 @@ const cors = require('cors')
 const userController = require('./controllers/User')
 const playlistController = require('./controllers/Playlist')
 const songController = require('./controllers/Song')
+const admin = require('./db/firebaseConfig')
 const app = express()
 
-
+class FirebaseAuth {
+    async authenticate(req, res, next) {
+        const token = req.headers.authorization.split(' ')[1];
+        try {
+            const decodeValue = await admin.auth().verifyIdToken(token);
+            if (decodeValue) {
+                console.log(decodeValue)
+                return next()
+            }
+            return res.json({ message: 'Unauthorized - stop trying to hack our playlists!!' }).sendStatus(401)
+        } catch (e) {
+            return res.json({ message: 'Internal Server Error' }).sendStatus(500)
+        }
+    }
+}
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.use(FirebaseAuth.authenticate)
 
 app.get('/', (req, res) => {
     res.redirect('/api/users')
