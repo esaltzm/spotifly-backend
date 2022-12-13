c718466531546996e1e8f86d7617aa5c85051f35
esaltzm <elisaltzman@gmail.com>
2022-12-13 17:08:21 -0500

    add default playlist cover image

--- models/Playlist.js
+++ models/Playlist.js
@@ -9,7 +9,11 @@ const playlistSchema = new Schema({
     songs: [{
         type: Schema.Types.ObjectId,
         ref: 'Song'
-    }]
+    }],
+    image: {
+        type: String,
+        default: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommunity.spotify.com%2Ft5%2FDesktop-Windows%2FPlaylist-cover-image-not-showing-up-on-playlists-with-mostly%2Ftd-p%2F986220&psig=AOvVaw3iNpkEmtS8dTEgijLYeChp&ust=1671055609287000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCMik57HN9_sCFQAAAAAdAAAAABAE'
+    }
 }, { timestamps: true })
 
 const Playlist = mongoose.model('Playlist', playlistSchema)
